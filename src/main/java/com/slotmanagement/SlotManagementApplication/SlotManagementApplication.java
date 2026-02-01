2cbb618576b03dbb619dd6201c84ae30124c1b34
rbhriegu459 <rbhriegu459@gmail.com>
2026-02-01 14:33:02 +0530

    project initialization

---
+++ src/main/java/com/slotmanagement/SlotManagementApplication/SlotManagementApplication.java
@@ -0,0 +1,13 @@
+package com.slotmanagement.SlotManagementApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slotmanagement/SlotManagementApplication/SlotManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slotmanagement.SlotManagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
